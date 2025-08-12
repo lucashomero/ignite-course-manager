2757853b142820a5de168d6477eef1befb052e8c
lucashomero <lucashomerovs@gmail.com>
2025-08-12 20:14:20 -0300

    initial commit

---
+++ ignite-course-manager/src/main/java/br/com/lucashomero/ignite_course_manager/IgniteCourseManagerApplication.java
@@ -0,0 +1,13 @@
+package br.com.lucashomero.ignite_course_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IgniteCourseManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IgniteCourseManagerApplication.class, args);
+	}
+
+}
---
+++ ignite-course-manager/src/test/java/br/com/lucashomero/ignite_course_manager/IgniteCourseManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lucashomero.ignite_course_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IgniteCourseManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
