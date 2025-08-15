d5c083b348343f550201459dca2e362fee2b142e
lucashomero <lucashomerovs@gmail.com>
2025-08-15 05:57:49 -0300

    initial commit

---
+++ ignite-course-manager/src/main/java/br/com/lucashomero/ignite_course_manager/controller/CursoController.java
@@ -0,0 +1,12 @@
+package br.com.lucashomero.ignite_course_manager.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cursos")
+public class CursoController {
+
+	
+	
+}
