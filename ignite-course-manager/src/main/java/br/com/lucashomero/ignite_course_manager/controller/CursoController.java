51845a068bf5aee9bf59cbc39c36f17018ab9a02
lucashomero <lucashomerovs@gmail.com>
2025-08-15 07:02:31 -0300

    code(controller): get method

--- ignite-course-manager/src/main/java/br/com/lucashomero/ignite_course_manager/controller/CursoController.java
+++ ignite-course-manager/src/main/java/br/com/lucashomero/ignite_course_manager/controller/CursoController.java
@@ -1,5 +1,7 @@
 package br.com.lucashomero.ignite_course_manager.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +9,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/cursos")
 public class CursoController {
 
-	
+	@GetMapping("/")
+	public ResponseEntity<String> getAllCourses(){
+		
+		return ResponseEntity.ok("deu ok");
+	}
 	
 }
---
+++ ignite-course-manager/src/main/java/br/com/lucashomero/ignite_course_manager/entity/CourseEntity.java
@@ -0,0 +1,18 @@
+//package br.com.lucashomero.ignite_course_manager.entity;
+//
+//import jakarta.persistence.Entity;
+//import lombok.AllArgsConstructor;
+//import lombok.Builder;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+//
+//
+//@Entity(name = "course")
+//@Builder
+//@Data
+//@AllArgsConstructor
+//@NoArgsConstructor
+//
+//public class CourseEntity {
+//
+//}
